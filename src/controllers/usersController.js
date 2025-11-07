5bb38b89307e8c97d4f54c63b406703d5a200b1f
keren0719 <subirozgalvankerensaray@gmail.com>
2025-11-06 21:58:08 -0500

    Refactor createUser function for better validation

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -34,22 +34,31 @@ async function getUserById(req, res) {
   }
 }
 
-async function createUser(req, res) {
-  const { name, email } = req.body;
-  if (!name || !email) return res.status(400).json({ message: 'Faltan campos (name, email)' });
+async function createUser(req, res) 
+{
+  if(!req.body)
+  {
+    return res.status(400).json({ message: 'Estructura incorrecta' });
+  }
 
-  try {
-    const [result] = await pool.query('INSERT INTO users (name, email) VALUES (?, ?)', [name, email]);
+  const { name, email, password, rol } = req.body;
+
+  if (!name || !email || !password || !rol) return res.status(400).json({ message: 'Faltan campos (name, email)' });
+  
+  try 
+  {
+    const [result] = await pool.query('INSERT INTO users (username, email,password,role_id,is_active) VALUES (?,?,?,?,?)', [name, email,password,rol,1]);
     const insertedId = result.insertId;
-    const [rows] = await pool.query('SELECT id, name, email, created_at FROM users WHERE id = ?', [insertedId]);
+    const [rows] = await pool.query('SELECT username, created_at FROM users WHERE id = ?', [insertedId]);
     res.status(201).json(rows[0]);
-  } catch (err) {
+  } catch (err) 
+  {
     console.error(err);
     // Manejo simple de error de clave única
     if (err.code === 'ER_DUP_ENTRY') {
       return res.status(409).json({ message: 'El email ya está registrado' });
     }
-    res.status(500).json({ message: 'Error al crear usuario' });
+    res.status(500).json({ message: 'Error al crear usuario: ' + err.message });
   }
 }
 
