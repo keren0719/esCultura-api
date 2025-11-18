e485f567876f7e800b4b6950edfe50c150570dc8
keren0719 <subirozgalvankerensaray@gmail.com>
2025-11-18 11:14:45 -0500

    version 2.0

---
+++ src/middlewares/upload.js
@@ -0,0 +1,18 @@
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/'); // crea carpeta "uploads"
+  },
+  filename: (req, file, cb) => {
+    const unique = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, unique + path.extname(file.originalname));
+  }
+});
+
+const upload = multer({
+  storage: storage
+});
+
+module.exports = { upload };
